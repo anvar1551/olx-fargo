3f5dc649c7bdb5fb4ae67ce05b54eca1c83b8ad8
Anvarbek Sharipov <anvarjon1551@gmail.com>
2023-07-24 16:21:14 +0500

    Telegram message added

--- server/server.js
+++ server/server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "https://olx-fargo.netlify.app");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
